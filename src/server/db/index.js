4a5be55c0b139d63b3b60f3ddac12a06257d0c7c
Hugo <contacthugocampos@gmail.com>
2019-07-07 20:14:38 -0700

    add line

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -2,6 +2,8 @@ const Sequelize = require('sequelize');
 const password = require('./pass');
 
 //Create Postgres DB in Terminal with: `createdb.exe -U postgres inventory`
+//const db = new Sequelize(process.env.DATABASE_URL || 'postgres://[username:password@]localhost[:post]/dbName')
+// const db = new Sequelize (dbName, username, password, {options})
 const db = new Sequelize('inventory', 'postgres', password, {
   dialect: 'postgres',
   host: process.env.HOST||'localhost',
